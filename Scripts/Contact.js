3021b080742692e57d0f7eeaababe2840800e0c7
tharshinisaravanan <tharshinisaravanan13@gmail.com>
2024-11-28 20:55:04 +0530

    contact

---
+++ Scripts/Contact.js
@@ -0,0 +1,18 @@
+let Btn = document.getElementById("btn");
+
+Btn.addEventListener("click",(event)=>{
+    let I1 = document.getElementById("i1").value;
+    let I2 = document.getElementById("i2").value;
+    let I3 = document.getElementById("i3").value;
+
+    let UserInfo = {
+        Name:I1,
+        Email:I2,
+        Msg:I3
+    }
+
+  
+    
+    
+    
+})
