1195af06f9ce4b9609859cac75204a490cf6a444
tharshinisaravanan <tharshinisaravanan13@gmail.com>
2024-12-02 18:48:30 +0530

    Ticketbooking

--- Scripts/Ticket.js
+++ Scripts/Ticket.js
@@ -3,103 +3,31 @@ Button.addEventListener("click",()=>{
     let i1 = document.getElementById("inp1").value;
     let i2 = document.getElementById("inp2").value;
 
-    if(i1.toLowerCase() == "chennai" && i2.toLowerCase() == "bangalore"){
-      let Res =   document.getElementById("result")
-      Res.innerHTML = `
-        <div class="col-sm-3" id="col12">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <img src="./Assets/bus1.jpg">
-              <h3>ABC Travels</h3>
-              <h4>From:Chennai</h4>
-              <h4>To:Bangalore</h4>
-            </div>
-          </div>
-        </div>
+   
+        
 
-        <div class="col-sm-3">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-            <img src="./Assets/bus2.jpg">
-              <h3>MEGHNA Travels</h3>
-              <h4>From:Chennai</h4>
-              <h4>To:Bangalore</h4>
-            </div>
-          </div>
-        </div>
+if(i1 == "chennai" && i2 == "bangalore"){
+  location = "./Ticketbooking.html"
+}
 
-        <div class="col-sm-3">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <img src="./Assets/bus3.jpg">
-                <h3>SRM Travels</h3>
-                <h4>From:Chennai</h4>
-                <h4>To:Bangalore</h4>
-              </div>
-          </div>
-        </div>
 
-        <div class="col-sm-3">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-            <img src="./Assets/bus4.jpg">
-              <h3>ASHWIN Transport</h3>
-              <h4>From:Chennai</h4>
-              <h4>To:Bangalore</h4>
-            </div>
-          </div>
-        </div>`
+
+
+    if(i1 == "" && i2 == ""){
+
+let Art = document.getElementById("art")
+Art.innerHTML = `
+<div class="alert alert-danger alert-dismissible fade show mt-5" role="alert">
+  <strong>Can't find Anything</strong> You should check in on some of those fields below.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>`
     }
 
-    if(i1.toLowerCase() == "tenkasi" && i2.toLowerCase() == "chennai")
-    {
-      let Res2 =   document.getElementById("result2")
-      Res2.innerHTML = `
-        <div class="col-sm-3" id="col12">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body2">
-              <img src="./Assets/bus1.jpg">
-              <h3>ABC Travels</h3>
-              <h4>From:Tenkasi</h4>
-              <h4>To:Chennai</h4>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-3">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body2">
-            <img src="./Assets/bus2.jpg">
-              <h3>MEGHNA Travels</h3>
-              <h4>From:Tenkasi</h4>
-              <h4>To:Chennai</h4>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-3">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body2">
-            <img src="./Assets/bus3.jpg">
-              <h3>SRM Travels</h3>
-              <h4>From:Tenkasi</h4>
-              <h4>To:Chennai</h4>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-3">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body2">
-            <img src="./Assets/bus4.jpg">
-              <h3>ASHWIN Transport</h3>
-              <h4>From:Tenkasi</h4>
-              <h4>To:Chennai</h4>
-            </div>
-          </div>
-        </div>`    
-    }
 })
+
+
+
+
 
 
 
---
+++ Scripts/Travelguide.js
@@ -0,0 +1,12 @@
+let Button = document.getElementById("btn")
+Button.addEventListener("click",()=>{
+    let i1 = document.getElementById("inp2").value;
+
+   
+        
+
+if(i1 == "bangalore"){
+  location = "Travelplace.html";
+}
+
+})
